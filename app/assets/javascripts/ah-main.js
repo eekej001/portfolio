f30b0606d8812f103b8e261e3e500bb0e360a9f6
Ben <eekej001@gmail.com>
2018-01-25 16:28:41 -0800

    added animated headline styling

--- app/assets/javascripts/ah-main.js
+++ app/assets/javascripts/ah-main.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function($){
 	//set animation timing
-	var animationDelay = 1500,
+	var animationDelay = 0,
 		//loading bar effect
-		barAnimationDelay = 800,
+		barAnimationDelay = 3800,
 		barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
 		//letters effect
 		lettersDelay = 50,
