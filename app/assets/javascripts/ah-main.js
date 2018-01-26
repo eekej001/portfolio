c0a52351b1c58768311caf6875e1d9fdce23eb74
Ben <eekej001@gmail.com>
2018-01-25 17:03:44 -0800

    added animated headline styling

--- app/assets/javascripts/ah-main.js
+++ app/assets/javascripts/ah-main.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function($){
 		//type effect
 		typeLettersDelay = 65,
 		selectionDuration = 200,
-		typeAnimationDelay = 300,
+		typeAnimationDelay = 200,
 		//clip effect 
 		revealDuration = 150,
 		revealAnimationDelay = 200;
